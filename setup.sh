7e4309aaa56c75948824b6c07bd484c51d7e51ee
Lý Quang Toại <115530159+lyquangtoaidz@users.noreply.github.com>
2022-10-27 23:04:08 +0700

    update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
-web='https://github.com/lyquangtoaidz/runtool/main/'
+web='https://lyquangtoai.dev'
 home='/data/data/com.termux/files/'
 printf '\n\033[1;32m Tiến Hành Cài Dữ Liệu\n\n'
 #cd ~
@@ -9,7 +9,7 @@ printf '\n\033[1;32m Tiến Hành Cài Python\n\n'
 #pkg install python
 #pip install --upgrade pip
 pip install requests
-curl -s $web/lyquangtoai -o $home/usr/bin/lyquangtoai
+curl -s $web/lyquangtoai.py -o $home/usr/bin/lyquangtoai
 chmod 777 $home/usr/bin/lyquangtoai
 
 printf '\n\033[1;32m Gõ \033[1;33mlyquangtoai \033[1;32mĐể Vào Tool <3\n\n'
